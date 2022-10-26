10ba25ac1398ad860327d039e433fd6b241594c0
RiA Min <72693650+noble9915@users.noreply.github.com>
2022-10-26 23:09:35 +0900

    code

---
+++ code/fMain.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MoneyBook
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btIN = new System.Windows.Forms.ToolStripButton();
+            this.btLogin = new System.Windows.Forms.ToolStripButton();
+            this.btOut = new System.Windows.Forms.ToolStripButton();
+            this.btDelect = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbUserName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lv1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btEdit = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btIN,
+            this.btLogin,
+            this.btOut,
+            this.btEdit,
+            this.btDelect});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(550, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btIN
+            // 
+            this.btIN.Image = ((System.Drawing.Image)(resources.GetObject("btIN.Image")));
+            this.btIN.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btIN.Name = "btIN";
+            this.btIN.Size = new System.Drawing.Size(63, 24);
+            this.btIN.Text = "입금";
+            this.btIN.Click += new System.EventHandler(this.btIN_Click);
+            // 
+            // btLogin
+            // 
+            this.btLogin.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btLogin.Image = ((System.Drawing.Image)(resources.GetObject("btLogin.Image")));
+            this.btLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(78, 24);
+            this.btLogin.Text = "로그인";
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // btOut
+            // 
+            this.btOut.Image = ((System.Drawing.Image)(resources.GetObject("btOut.Image")));
+            this.btOut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btOut.Name = "btOut";
+            this.btOut.Size = new System.Drawing.Size(63, 24);
+            this.btOut.Text = "출금";
+            this.btOut.Click += new System.EventHandler(this.btOut_Click);
+            // 
+            // btDelect
+            // 
+            this.btDelect.Image = ((System.Drawing.Image)(resources.GetObject("btDelect.Image")));
+            this.btDelect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btDelect.Name = "btDelect";
+            this.btDelect.Size = new System.Drawing.Size(63, 24);
+            this.btDelect.Text = "삭제";
+            this.btDelect.Click += new System.EventHandler(this.btDelect_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.sbUserName});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 410);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(550, 25);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(54, 20);
+            this.toolStripStatusLabel1.Text = "사용자";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // sbUserName
+            // 
+            this.sbUserName.Name = "sbUserName";
+            this.sbUserName.Size = new System.Drawing.Size(18, 20);
+            this.sbUserName.Text = "...";
+            this.sbUserName.Click += new System.EventHandler(this.sbUserName_Click);
+            // 
+            // lv1
+            // 
+            this.lv1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lv1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lv1.FullRowSelect = true;
+            this.lv1.GridLines = true;
+            this.lv1.HideSelection = false;
+            this.lv1.Location = new System.Drawing.Point(0, 27);
+            this.lv1.MultiSelect = false;
+            this.lv1.Name = "lv1";
+            this.lv1.Size = new System.Drawing.Size(550, 383);
+            this.lv1.TabIndex = 2;
+            this.lv1.UseCompatibleStateImageBehavior = false;
+            this.lv1.View = System.Windows.Forms.View.Details;
+            this.lv1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "날짜";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "분류";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "입금";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "출금";
+            this.columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "비고";
+            this.columnHeader5.Width = 250;
+            // 
+            // btEdit
+            // 
+            this.btEdit.Image = ((System.Drawing.Image)(resources.GetObject("btEdit.Image")));
+            this.btEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btEdit.Name = "btEdit";
+            this.btEdit.Size = new System.Drawing.Size(63, 24);
+            this.btEdit.Text = "편집";
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 435);
+            this.Controls.Add(this.lv1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "가계부";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btIN;
+        private System.Windows.Forms.ToolStripButton btOut;
+        private System.Windows.Forms.ToolStripButton btLogin;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel sbUserName;
+        private System.Windows.Forms.ListView lv1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ToolStripButton btDelect;
+        private System.Windows.Forms.ToolStripButton btEdit;
+    }
+}
+
